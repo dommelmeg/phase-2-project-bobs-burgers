3c3fcd7a0207651e39882a0e4f0e90043279028a
Megan <dommelmeg@gmail.com>
2023-02-01 22:03:25 -0500

    adds search bar to character pg

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CharacterCard from "../components/CharacterCard";
-import { Image, Box, SimpleGrid, Stack, Heading } from '@chakra-ui/react'
+import { Image, Box, SimpleGrid, Stack, Heading, InputGroup, Input, InputLeftElement } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
 
 const Characters = ({ nineCharacters, allCharacters }) => {
   
@@ -9,6 +10,13 @@ const Characters = ({ nineCharacters, allCharacters }) => {
       <Box>
         <Heading color='yellow.500' size='2xl'>Characters</Heading>
       </Box>
+      <InputGroup>
+    <InputLeftElement
+      pointerEvents='none'
+      children={<SearchIcon color='gray.300' />}
+    />
+    <Input type='tel' placeholder='Character Name' />
+  </InputGroup>
       <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(400px, 1fr))'>
         {nineCharacters.map((character) => {
           return (
