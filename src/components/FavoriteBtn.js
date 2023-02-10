f29f988da4b2bdac00e1085cd0b28c7446729506
Megan <dommelmeg@gmail.com>
2023-02-09 19:49:44 -0500

    fixes fav btn bug

--- src/components/FavoriteBtn.js
+++ src/components/FavoriteBtn.js
@@ -4,13 +4,14 @@ import { FavoriteCharacterContext } from "../context/favoriteCharacters";
 
 const FavoriteBtn = ({ character }) => {
   const { favoriteCharacters, setFavoriteCharacters } = useContext(FavoriteCharacterContext)
-  const isFavorite = favoriteCharacters.includes(character)
+  const favCharacterIds = favoriteCharacters.map(favCharacter => favCharacter.id)
+  const isFavorite = favCharacterIds.includes(character.id)
 
   const handleFavoriteBtn = () => {
     if (!isFavorite) {
       setFavoriteCharacters([...favoriteCharacters, character])
     } else {
-      const filterFavorites = favoriteCharacters.filter((favoriteCharacter) => favoriteCharacter !== character)
+      const filterFavorites = favoriteCharacters.filter((favoriteCharacter) => favoriteCharacter.id !== character.id)
       setFavoriteCharacters(filterFavorites)
     }
   }
